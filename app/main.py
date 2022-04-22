94bf4c10c7fee5d451e29bfa622aa5924ef2172f
jaroslaw.borelowski <jaroslaw.borelowski@infosys.com>
2022-04-22 11:32:35 +0200

    if__name__ ... added

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI 
+import uvicorn
+from uvicorn.config import Config
+from fastapi import FastAPI
 
 app = FastAPI(
     title="FastAPI - Hello World",
@@ -10,3 +12,16 @@ app = FastAPI(
 @app.get("/")
 def hello_world():
     return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    if __name__ == "__main__":
+        port = 8000
+        print(f"Starting GONZO service on port {port}")
+        uvicorn.Server(config=(
+            Config(app=app,
+                   port=port,
+                   host='0.0.0.0',
+                   reload=False)
+        )).run()
+
